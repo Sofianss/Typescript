667bdfc6bdfc3a0e6085707005e5a8bc3656d88a
Sofiane Salhi <sofiane.slhi@gmail.com>
2022-11-24 15:10:17 +0100

    feat: Exercice 3

--- Exercice 3.ts
+++ Exercice 3.ts
@@ -1,14 +1,19 @@
-
+/**
+ * Calcul le bénéfice ou la perte d'une transaction en fonction de son cout et  de son prix de vente
+ * @param Pdv prix de vente du produit
+ * @param Cdf prix d'achat du produit
+ * @returns Une phrase qui indique le bénéfice/la perte
+ */
 function Benefice(Pdv: number, Cdf: number) : string {
     const benefice: number = Pdv - Cdf
     if (benefice < 0){
-        return `Perte de ${-benefice}`
+        return `Perte de ${-benefice}`;
     } else if (benefice > 0) {
-        return `Gain de ${benefice}`
+        return `Gain de ${benefice}`;
     } else {
-        return `Vive le bénévolat`
+        return `Vive le bénévolat`;
     }
 }
-console.log(Benefice(10,20))
-console.log(Benefice(20,10))
-console.log(Benefice(10,10))
+console.log(Benefice(10,20));
+console.log(Benefice(20,10));
+console.log(Benefice(10,10));
