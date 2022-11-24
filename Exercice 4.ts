9246c8fa9991e3050d2d548ac27d7c59a74dd3e9
Sofiane Salhi <sofiane.slhi@gmail.com>
2022-11-24 16:49:27 +0100

    Docs: Exercice 4

---
+++ Exercice 4.ts
@@ -0,0 +1,14 @@
+function PlusGrandNombre(nombre1: number, nombre2: number, nombre3: number) : string {
+    if (nombre1 > nombre2 ){
+        return `Le plus grand nombre est : ${nombre1}`;
+    } else if (nombre2 > nombre3) {
+        return `Le plus grand nombre est : ${nombre2}`;
+    } else if (nombre3 > nombre1 && nombre2) {
+        return `Le plus grand nombre est : ${nombre3}`;
+    }
+    else {
+        return "Veuillez rentrer un nombre"
+    }
+   
+}
+console.log(PlusGrandNombre(800,700,600));
