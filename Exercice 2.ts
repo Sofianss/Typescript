6bb9e2376238791133598b20433653981ae9b08e
Sofiane Salhi <sofiane.slhi@gmail.com>
2022-11-24 13:32:19 +0100

    Docs: Exercice 2

---
+++ Exercice 2.ts
@@ -0,0 +1,11 @@
+const nombre1 : number = 5;
+const nombre2 : number = 8;
+/**
+ * Fonction pour faire une somme
+ */
+function calcul() : void {
+
+}
+
+console.log(` ${nombre1} + ${nombre2} = ${nombre1 + nombre2}`);
+
