5745eaca2bf6866f13f7250f909c72353c802fc5
Sofiane Salhi <sofiane.slhi@gmail.com>
2022-11-28 09:23:24 +0100

    Docs: Exercice2

--- Exercice 2.ts
+++ Exercice 2.ts
@@ -1,11 +1,13 @@
-const nombre1 : number = 5;
-const nombre2 : number = 8;
-/**
- * Fonction pour faire une somme
- */
-function calcul() : void {
 
+/**
+ * Réalisation d'une addition à partir de deux nombres.
+ * @param nombre1 premier nombre
+ * @param nombre2 premier nombre
+ */
+function addition(nombre1: number, nombre2: number) : void {
+const resultat: number = nombre1 + nombre2
+console.log(resultat);
 }
 
-console.log(` ${nombre1} + ${nombre2} = ${nombre1 + nombre2}`);
+addition(5,5);
 
